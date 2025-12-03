b3f7aa8cbaccdb4ffb7994a9958697c784c463db
Florent <florent.sylejmani@student.uni-pr.edu>
2025-12-03 10:32:53 +0100

    feat: add UpdateItemQuantity component, enhance cart item handling, and improve order processing

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,17 +1,22 @@
-import { formatCurrency } from '../../utils/helpers';
-import DeleteItem from './DeleteItem';
+import { useSelector } from "react-redux";
+import { formatCurrency } from "../../utils/helpers";
+import DeleteItem from "./DeleteItem";
+import UpdateItemQuantity from "./UpdateItemQuantity";
+import { getCurerrentQuantityById } from "./cartSlice";
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
+  const currentQuantity = useSelector(getCurerrentQuantityById(pizzaId));
 
   return (
-    <li className='py-3 sm:flex sm:items-center sm:justify-between'>
-      <p className='mb-1 sm:mb-0'>
+    <li className="py-3 sm:flex sm:items-center sm:justify-between">
+      <p className="mb-1 sm:mb-0">
         {quantity}&times; {name}
       </p>
-      <div className='flex justify-between items-center sm:gap-6'>
-        <p className='text-sm font-bold'>{formatCurrency(totalPrice)}</p>
-        <DeleteItem pizzaId={pizzaId}/>
+      <div className="flex items-center justify-between sm:gap-6">
+        <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+        <UpdateItemQuantity pizzaId={pizzaId} currentQuantity={currentQuantity} />
+        <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
   );
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -3,13 +3,14 @@ import Button from "../../ui/Button";
 import { useDispatch, useSelector } from "react-redux";
 import { addItem, getCurerrentQuantityById } from "../cart/cartSlice";
 import DeleteItem from "../cart/DeleteItem";
+import UpdateItemQuantity from "../cart/UpdateItemQuantity";
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
 
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
   const currentQuantity = useSelector(getCurerrentQuantityById(id));
-  const isInCart = currentQuantity >0;
+  const isInCart = currentQuantity > 0;
 
   // console.log(id)
   function handleAddToCart() {
@@ -44,7 +45,15 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          {isInCart && <DeleteItem pizzaId={id} />}
+          {isInCart && (
+            <div className="flex items-center gap-3 sm:gap-8">
+              <UpdateItemQuantity
+                pizzaId={id}
+                currentQuantity={currentQuantity}
+              />
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
 
           {!soldOut && !isInCart && (
             <Button type="small" onClick={handleAddToCart}>
